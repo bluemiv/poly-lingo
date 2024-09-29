5407e0d847a6140b85706ddc6b0043638ee7e025
taehongkim <public.bluemiv@gmail.com>
2024-09-29 21:57:20 +0900

    feat: [FE] antd & Tailwindcss 설정 #3

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { StyleProvider } from '@ant-design/cssinjs';
+import App from './App.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <StyleProvider hashPriority="high">
+      <App />
+    </StyleProvider>
   </StrictMode>,
-)
+);
