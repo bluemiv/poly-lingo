903d8753c1d39e8c471d473cd9d8302aa8e5dcfb
taehongkim <public.bluemiv@gmail.com>
2024-10-03 20:18:44 +0900

    feat: [BE] mongodb 연동 #7

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -3,9 +3,30 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TranslationModule } from './translation/translation.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TranslationModule, MongooseModule.forRoot('mongodb://root:1234@localhost:27017')],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const username = configService.get<string>('MONGODB_USERNAME');
+        const password = configService.get<string>('MONGODB_PASSWORD');
+        const host = configService.get<string>('MONGODB_HOST');
+        const port = configService.get<string>('MONGODB_PORT');
+        const database = configService.get<string>('MONGODB_DATABASE');
+
+        const uri = `mongodb://${username}:${password}@${host}:${port}/${database}`;
+
+        return {
+          uri,
+        };
+      },
+    }),
+    TranslationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
